e529ba5656c132e428358e70d2b60e7734fd37e6
Sajeesh V <sajeesh.v.tblr@gmai.com>
2024-07-05 17:54:58 +0530

    first commit

---
+++ add_vertex.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
+)
+
+// GremlinClient is a struct that holds the Gremlin client
+type GremlinClient struct {
+	client *gremlingo.Client
+}
+
+// newGremlinClient initializes and returns a new GremlinClient
+func newGremlinClient() (*GremlinClient, error) {
+	gremlinClient, err := gremlingo.NewClient("ws://localhost:8182/gremlin")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Gremlin client: %v", err)
+	}
+	return &GremlinClient{client: gremlinClient}, nil
+}
+
+// AddVertex adds a vertex with the given properties to the graph
+func AddVertex(label string, properties map[string]interface{}) ([]map[string]interface{}, error) {
+	// Initialize the Gremlin client
+	gc, err := newGremlinClient()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Gremlin client: %v", err)
+	}
+	// Ensure the client is closed when the function returns
+	defer gc.client.Close()
+
+	// Construct the Gremlin traversal string
+	traversalString := fmt.Sprintf(`g.addV('%s')`, label)
+	for key, value := range properties {
+		traversalString += fmt.Sprintf(`.property('%s', '%v')`, key, value)
+	}
+
+	// Execute the traversal
+	res, err := gc.client.Submit(traversalString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute traversal: %v", err)
+	}
+	var resultsMap []map[string]interface{}
+	// results := res.GetStatusAttributes()
+	log.Printf("ffetre %v", res)
+	return resultsMap, nil
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module janus-graph-with-go
+
+go 1.22.4
+
+require (
+	github.com/apache/tinkerpop/gremlin-go/v3 v3.7.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/nicksnyder/go-i18n/v2 v2.4.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
